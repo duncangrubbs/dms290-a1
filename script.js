8f2e1cabd1ee3c6c3545163dcd56120ca8daf058
Duncan Grubbs <duncan.grubbs@gmail.com>
2021-02-11 16:12:34 -0500

    compress images and fix styling.

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-function scrollUp(event) {
+function scrollUp() {
   window.scrollTo({top: 0, behavior: 'smooth'});
+}
+
+function bookTrip() {
+  window.location.href = 'https://www.expedia.com/Copenhagen.d178252.Destination-Travel-Guides';
 }
